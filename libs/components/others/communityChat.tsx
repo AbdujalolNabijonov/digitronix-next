b76ff0b70a21daf073bd4670570ab02ded4dda77
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2025-02-19 00:57:42 +0900

    feat: Emoji feature adding.

--- libs/components/others/communityChat.tsx
+++ libs/components/others/communityChat.tsx
@@ -1,18 +1,22 @@
 import { RippleBadge } from "@/scss/MaterialTheme/styled"
-import { Forum, Message, Send } from "@mui/icons-material"
+import { EmojiEmotions, Forum, Send } from "@mui/icons-material"
 import { Button, CssVarsProvider, Input } from "@mui/joy"
-import { Avatar, Box, Divider, IconButton, Menu, MenuItem, Stack } from "@mui/material"
+import { Avatar, Box, Divider, IconButton, Menu, Stack } from "@mui/material"
 import moment from "moment"
 import { useEffect, useState } from "react"
 import { useReactiveVar } from "@apollo/client"
 import { socketVar, userVar } from "@/apollo/store"
 import { Messages, serverApi } from "@/libs/config"
 import { sweetErrorHandling } from "@/libs/sweetAlert"
+import EmojiPicker from "emoji-picker-react"
+import useDeviceDetect from "@/libs/hooks/useDeviceDetector"
 
 const CommunityChat = () => {
     const [anchorEl, setAnchorEl] = useState(null)
+    const [anchorEl1, setAnchorEl1] = useState(null)
     const socket = useReactiveVar(socketVar)
     const user = useReactiveVar(userVar)
+    const device = useDeviceDetect()
     const [messages, setMessages] = useState<any[]>([])
     const [totalClients, setTotalClients] = useState(0)
     const [messageObj, setMessageObj] = useState({
@@ -26,7 +30,13 @@ const CommunityChat = () => {
             setAnchorEl(null)
         }
     }
-
+    const toogleAnchorHandler = (e: any) => {
+        if (!anchorEl1) {
+            setAnchorEl1(e.target)
+        } else {
+            setAnchorEl1(null)
+        }
+    }
     useEffect(() => {
         socket.onmessage = ({ data }) => {
             const payload = JSON.parse(data as string);
@@ -34,7 +44,7 @@ const CommunityChat = () => {
                 setTotalClients(payload.totalClients);
             }
             if (payload.event === "messages") {
-                setMessages(payload.messages)
+                setMessages([...payload.messages])
             }
             if (payload.event === "message") {
                 messages.push(payload)
@@ -48,8 +58,8 @@ const CommunityChat = () => {
             if (!messageObj.data) throw new Error(Messages.error3)
             const jsonMsg = JSON.stringify(messageObj)
             socket.send(jsonMsg)
-            messageObj.data=""
-            setMessageObj({...messageObj})
+            messageObj.data = ""
+            setMessageObj({ ...messageObj })
         } catch (err: any) {
             await sweetErrorHandling(err)
         }
@@ -64,7 +74,7 @@ const CommunityChat = () => {
                 open={Boolean(anchorEl)}
                 onClose={toggleMenuHandler}
             >
-                <Stack  className="menu-item">
+                <Stack className="menu-item">
                     <Stack>
                         <Stack className="menu-title" justifyContent={"center"}>
                             <Box>Online Chat</Box>
@@ -102,7 +112,7 @@ const CommunityChat = () => {
                                         return (
                                             <Stack className="msg-middle" flexDirection={"row"}>
                                                 <Divider sx={{ borderBottomColor: "white", flex: 1 }} />
-                                                <Box>{message.memberData?.memberNick??"GUEST"} has connected</Box>
+                                                <Box>{message.memberData?.memberNick ?? "GUEST"} has connected</Box>
                                                 <Divider sx={{ borderBottomColor: "white", flex: 1 }} />
                                             </Stack>
                                         )
@@ -131,8 +141,37 @@ const CommunityChat = () => {
                                         >
                                         </Button>
                                     }
+                                    startDecorator={
+                                        device === "mobile" ? null : (
+                                            <Button
+                                                variant="soft"
+                                                color="neutral"
+                                                onClick={toogleAnchorHandler}
+                                                startDecorator={<EmojiEmotions sx={{ fill: "gray", fontSize: '25px' }} />}
+                                            >
+                                            </Button>
+                                        )
+                                    }
                                 />
                             </CssVarsProvider>
+                            {
+                                device === "mobile" ? null : (
+                                    <Menu
+                                        anchorEl={anchorEl1}
+                                        open={Boolean(anchorEl1)}
+                                        onClose={toogleAnchorHandler}
+                                        sx={{ height: "350px", width: "350px" }}
+                                    >
+                                        <EmojiPicker
+                                            onEmojiClick={(emoji) => {
+                                                messageObj.data += emoji.emoji
+                                                setMessageObj({ ...messageObj })
+                                                setAnchorEl1(null)
+                                            }}
+                                        />
+                                    </Menu>
+                                )
+                            }
                         </Stack>
                     </Stack>
                 </Stack>
